1fa7b515f8a6edef2ff21bcd6b2ed27d701a509f
Daniel Danis <daniel.danis@jax.org>
2021-11-29 23:44:31 -0500

    Add stub for `phenofx-web`

---
+++ phenofx-web/src/main/java/org/monarchinitiative/phenofx/web/PhenoFxWeb.java
@@ -0,0 +1,13 @@
+package org.monarchinitiative.phenofx.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PhenoFxWeb {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PhenoFxWeb.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Running PhenoFX WEB");
+    }
+}
